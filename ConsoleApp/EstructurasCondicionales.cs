20d3a5953e00686967ee108f57d0c1ac6cc8ad8d
Sugey Katherine Malte Beltran <smalte@sistran.com.co>
2023-08-01 18:31:57 +0200

    first commit

---
+++ ConsoleApp/EstructurasCondicionales.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class EstructurasCondicionales
+    {
+        void Ejercicios(){
+            Console.WriteLine("Determinar par");
+            Console.WriteLine("Años bisiestos");
+            Console.WriteLine("División");
+            Console.WriteLine("Palabra más larga");
+            Console.WriteLine("Ordenamiento");
+            Console.WriteLine("Letra o número");
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("Edad");
+            Console.WriteLine("Set de tenis");
+            Console.WriteLine("Triángulos");
+            Console.WriteLine("Índice de masa corporal");
+        }
+    }
+}
---
+++ ConsoleApp/Simple.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Simple
+    {
+
+        void simples() {
+            bool salir = false;
+            while (!salir)
+            {
+
+                Console.WriteLine("1. saludo");
+                Console.WriteLine("2. cículos");
+                Console.WriteLine("3. promedio de notas");
+                Console.WriteLine("4. convertir de unidades de longitud");
+                Console.WriteLine("5. Número invertido");
+                Console.WriteLine("6. Pitágoras");
+                Console.WriteLine("7. Hora futura");
+                Console.WriteLine("8. Parte decimal");
+                Console.WriteLine("9. Qué nota necesito");
+                Console.WriteLine("10. Huevos a la copa");
+                Console.WriteLine("11. Salir");
+                Console.WriteLine("Elige una de las opciones");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Has elegido la opción 1");
+                        string nombrePersona = "";
+                        string texto = "";
+
+                        nombrePersona = Console.ReadLine();
+                        Console.WriteLine("buenos días: {0}", nombrePersona);
+                        System.Console.WriteLine("");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Has elegido la opción 2");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Has elegido la opción 3");
+                        break;
+                    case 4:
+                        Console.WriteLine("Has elegido la opción 3");
+                        break;
+                    case 5:
+                        Console.WriteLine("Has elegido la opción 3");
+                        break;
+                    case 6:
+                        Console.WriteLine("Has elegido la opción 3");
+                        break;
+                    case 7:
+                        Console.WriteLine("Has elegido la opción 3");
+                        break;
+                    case 8:
+                        Console.WriteLine("Has elegido la opción 3");
+                        break;
+                    case 9:
+                        Console.WriteLine("Has elegido la opción 3");
+                        break;
+                    case 10:
+                        Console.WriteLine("Has elegido la opción 3");
+                        break;
+                    case 11:
+                        Console.WriteLine("Has elegido salir de la aplicación");
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Elige una opcion entre 1 y 10");
+                        break;
+
+                }
+            }
+
+        }
+
+
+    }
+}
